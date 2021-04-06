d6faedfe8b9f614df7e2d7e1599c463a148c3343
IgorKhorev1 <igor.horew@ya.ru>
2021-04-06 23:19:54 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,25 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int num1;
+        int num2;
+
+        Scanner scanner = new Scanner(System.in);
+
+        num1 = scanner.nextInt();
+        num2 = scanner.nextInt();
+
+
+        if(num1>num2){
+            System.out.println("Число "+num1+" больше "+num2);
+        }
+        else {
+            System.out.println("Число "+num2+" больше "+num1);
+        }
+        System.out.println(num1+num2);
+    }
+}
