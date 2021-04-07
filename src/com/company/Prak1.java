b412d8e58571a9e78b97e5cca78aeb47fe641973
IgorKhorev1 <igor.horew@ya.ru>
2021-04-07 15:50:27 +0300

    Prak2 started

--- src/com/company/Prak1.java
+++ src/com/company/Prak1.java
@@ -2,7 +2,7 @@ package com.company;
 
 import java.util.Scanner;
 
-public class Main {
+public class Prak1 {
 
     public static void main(String[] args) {
         int num1;
--- src/com/company/Prak2.java
+++ src/com/company/Prak2.java
@@ -1,8 +1,19 @@
 package com.company;
 
+import java.util.Random;
+
 public class Prak2 {
     public static void main(String[] args) {
-        int[] array = new int[10];
+
+        int[] mass = new int[10];
+        Random random = new Random();
+        for (int i = 0; i < mass.length; i++) {
+            mass[i]= random.nextInt();
+        }
+
+        for (int j : mass) {
+            System.out.print(j + " ");
+        }
 
     }
 }
