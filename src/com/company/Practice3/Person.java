e981690df1fbfd662d57544858f58eee5080980b
IgorKhorev1 <igor.horew@ya.ru>
2021-04-12 19:29:58 +0300

    -

--- src/com/company/Practice3/Person.java
+++ src/com/company/Practice3/Person.java
@@ -1,4 +1,23 @@
 package com.company.Practice3;
 
 public class Person {
+    String firstName = new String();
+    String lastName = new String();
+
+    public Person (String firstName, String lastName){
+        this.firstName=firstName;
+        this.lastName=lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String toString() {
+        return String.format("Person \nlastName - %s\nfirstName - %s",lastName,firstName);
+    }
 }
---
+++ src/com/company/Practice3/Practice3.java
@@ -0,0 +1,20 @@
+package com.company.Practice3;
+
+public class Practice3 {
+    public static void main(String[] args) {
+        String[] firstNamesPerson = new String[]{"Ivan","Nikita","Dima","Ilya","Viktor"};
+        String[] lastNamesPerson = new String[]{"Kazakov","Kartoshkin","Mironov","Getmanchuk","Kuzin","Shabanov"};
+
+        Person[] randomPerson = new Person[50];
+        for (int i = 0; i < randomPerson.length; i++) {
+            int randomIndexFirstName = (int)(Math.random()*firstNamesPerson.length);
+            int randomIndexLastName = (int)(Math.random()*lastNamesPerson.length);
+            randomPerson[i]=new Person(firstNamesPerson[randomIndexFirstName],lastNamesPerson[randomIndexLastName]);
+
+            System.out.println(String.format("Person №%s - lastName - %s, firstName - %s",i,randomPerson[i].lastName,randomPerson[i].firstName));
+        }
+
+
+
+    }
+}
