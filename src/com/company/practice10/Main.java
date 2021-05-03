2ac32b9c65adeac13e37288cce94124465ecf9d9
IgorKhorev1 <igor.horew@ya.ru>
2021-05-03 22:09:31 +0300

    -

--- src/com/company/practice10/Main.java
+++ src/com/company/practice10/Main.java
@@ -2,7 +2,6 @@ package com.company.practice10;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
