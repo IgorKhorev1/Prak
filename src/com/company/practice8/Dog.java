078a1bcaaaf11acc1d7372f6994dee4ae1a0ca01
rkolesnikov <rkolesnikov@avaya.com>
2021-05-06 14:51:33 +0300

    practice 8

---
+++ src/com/company/practice8/Dog.java
@@ -0,0 +1,13 @@
+package com.company.practice8;
+
+public class Dog {
+    @JsonName("name")
+    public String firstName;
+    @JsonName("years")
+    public int age;
+
+    public Dog(String firstName, int age) {
+        this.firstName = firstName;
+        this.age = age;
+    }
+}
